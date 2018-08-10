2d60a9b99f6bac8bb2a8fd0df80631c4c9e6fe7a
YangChen0930 <945402563@qq.com>
2018-08-10 17:55:11 +0800

    music app init

---
+++ src/components/rank/rank.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        排行页
+    </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/recommend/recommend.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        推荐页
+    </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+</style>
