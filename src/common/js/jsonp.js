b1c6bff4d9ccd038ace38a6a582e23408acef352
YangChen0930 <945402563@qq.com>
2018-08-18 13:34:57 +0800

    slider component

---
+++ src/common/js/jsonp.js
@@ -0,0 +1,31 @@
+import originJSONP from 'jsonp'
+
+/**
+ *
+ *
+ * @export
+ * @param {*} url 纯净的url地址
+ * @param {*} data 请求参数
+ * @param {*} option
+ */
+export default function jsonp(url, data, option) {
+  url += (url.indexOf('?') < 0 ? '?' : '&') + params(data)
+  return new Promise((resolve, reject) => {
+    originJSONP(url, option, (err, data) => {
+      if (!err) {
+        resolve(data)
+      } else {
+        reject(err)
+      }
+    })
+  })
+}
+
+function params(data) {
+  let url = ''
+  for (var k in data) {
+    let value = data[k] !== undefined ? data[k] : ''
+    url += `&${k}=${encodeURIComponent(value)}`
+  }
+  return url ? url.substring(1) : ''
+}
--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,12 +1,71 @@
 <template>
-    <div>
-        推荐页
+    <div class="recommend">
+        <div class="recommend-content">
+            <div v-if="recommends.length" class="slider-wrapper">
+                <slider>
+                    <div v-for="(item,index) in recommends" :key="index">
+                        <a :href="item.linkUrl">
+                            <img :src="item.picUrl" alt="">
+                        </a>
+                    </div>
+                </slider>
+            </div>
+            <div class="recommend-list">
+                <h1 class="list-title">热门歌单推荐</h1>
+                <ul></ul>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-export default {}
+import slider from 'base/slider/slider'
+import { getRecommend } from 'api/recommend'
+import { ERR_OK } from 'api/config'
+
+export default {
+  data() {
+    return {
+      recommends: []
+    }
+  },
+  created() {
+    this._getRecommend()
+  },
+  methods: {
+    _getRecommend() {
+      getRecommend().then(res => {
+        if (res.code === ERR_OK) {
+          this.recommends = res.data.slider
+        }
+      })
+    }
+  },
+  components: {
+    slider
+  }
+}
 </script>
 
-<style scoped>
+<style lang="stylus">
+@import '~common/stylus/variable'
+.recommend
+    position: fixed
+    width: 100%
+    top: 88px
+    bottom: 0
+    .recommend-content
+        height: 100%
+        overflow: hidden
+        .slider-wrapper
+            position: relative
+            width: 100%
+            overflow: hidden
+        .recommend-list
+            .list-title
+                height: 65px
+                line-height: 65px
+                text-align: center
+                font-size: $font-size-medium
+                color: $color-theme
 </style>
