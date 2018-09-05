7762af5b5ddd370f5b65993ca7deeaef8d6c99e4
YangChen0930 <945402563@qq.com>
2018-09-05 13:44:21 +0800

    user-center component

--- src/store/actions.js
+++ src/store/actions.js
@@ -9,7 +9,9 @@ import {
   saveSearch,
   deleteSearch,
   clearSearch,
-  savePlay
+  savePlay,
+  saveFavorite,
+  deleteFavorite
 } from 'common/js/cache'
 
 function findIndex(list, song) {
@@ -155,4 +157,12 @@ export const deleteSongList = function ({
 
 export const savePlayHistory = function ({commit}, song) {
   commit(types.SET_PLAY_HISTORY, savePlay(song))
+}
+
+export const saveFavoriteList = function ({commit}, song) {
+  commit(types.SET_FAVORITE_LIST, saveFavorite(song))
+}
+
+export const deleteFavoriteList = function ({commit}, song) {
+  commit(types.SET_FAVORITE_LIST, deleteFavorite(song))
 }
